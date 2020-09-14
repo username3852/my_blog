1d1d36e4d393be67cf031c7f840eb9ec28f0ef56
username3852 <username3852@gmail.com>
2020-09-14 14:19:36 +0545

    User Login and Logout

--- project/settings.py
+++ project/settings.py
@@ -130,3 +130,4 @@ STATICFILES_DIRS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'  # default after login
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegisterForm
 
+# to avoid unauthorized users to access via urls directly
+from django.contrib.auth.decorators import login_required
+
 # this is to create flash message appear after registeration is complete
 from django.contrib import messages
 
@@ -29,3 +32,8 @@ def register(request):
     else:
         form = UserRegisterForm()  # i.e. GET method or request
         return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
